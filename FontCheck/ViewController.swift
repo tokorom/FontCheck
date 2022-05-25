84ab65be301e9bcb72f7dfc59ebc90cc226f24b2
tokorom <tokorom@gmail.com>
2022-05-25 13:00:48 +0900

    First commit

---
+++ FontCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FontCheck
+//
+//  Created by Yuta Tokoro on 2022/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
